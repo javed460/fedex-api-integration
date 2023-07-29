9042f28339b2231b1afe46cefdcf46f040107676
Javed Bagwan <javed460@gmail.com>
2023-07-29 11:09:55 +0530

    Initial checkin

---
+++ src/main/java/com/example/fedexintegration/FedexIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.fedexintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedexIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedexIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fedexintegration/FedexIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fedexintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedexIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
