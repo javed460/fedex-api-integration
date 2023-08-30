bafa4e5650d8ec148a646bf429dced49135d6e33
Javed Bagwan <javed460@gmail.com>
2023-08-30 09:31:30 +0530

    Added WeightDTO

---
+++ src/main/java/com/example/fedexintegration/dto/WeightDTO.java
@@ -0,0 +1,13 @@
+package com.example.fedexintegration.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class WeightDTO {
+    private String units;
+    private double value;
+}
