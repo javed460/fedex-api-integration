8e58c0e8a8394bfc777ef8a91d8feb1a2e927852
Javed Bagwan <javed460@gmail.com>
2023-09-04 07:59:04 +0530

    Added PackageDetailDTO

---
+++ src/main/java/com/example/fedexintegration/dto/pickup/request/PackageDetailDTO.java
@@ -0,0 +1,12 @@
+package com.example.fedexintegration.dto.pickup.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PackageDetailDTO {
+    private PackageSpecialServicesDTO packageSpecialServices;
+}
